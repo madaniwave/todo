af4b5979674766f674543d2f99ef409c601f1fc2
madaniwave <daffamadani845@gmail.com>
2025-11-09 22:33:16 -0800

    update

--- tugas-pak-hadi-safar/zakat.js
+++ tugas-pak-hadi-safar/zakat.js
@@ -7,20 +7,21 @@ var hargaEmasRupiah = 1100000;
 
 
 function muatHistory(){
-   let data=localStorage.getItem(`zakatHistory`); 
+   let data= localStorage.getItem(`zakatHistory`); // untuk mengambil 
    if(data === null){
     data =[];
    }else{
     data = JSON.parse(data);
    }
    historyList.innerHTML=""; 
-   for (var i=0; i<data.length; i++){
-    var li = document.createElement("li");
-    li.innerHTML = `<span> ${data[i]} </span>`; 
-    historyList.appendChild(li);
+   for (var i=0; i<data.length; i++){ 
+                   //jumlah elemn dlm array data
+    var li = document.createElement("li"); // untuk menghasilkan node DOM kosong 
+    li.innerHTML = `<span> ${data[i]} </span>`; // untuk mengisi elemen li dengan teks dari data[i] dibungkus di dalam span
+    historyList.appendChild(li); // untk menmbahkan li kedalm historylist
    }
 }
-function simpanHistory(text){
+function simpanHistory(text){ // function untuk mneyalakan
     let data=localStorage.getItem('zakatHistory');
     if(data === null){
         data =[]; 
@@ -32,18 +33,18 @@ function simpanHistory(text){
         data = data.slice(0,10);
     }
     localStorage.setItem('zakatHistory', JSON.stringify(data)) 
-    muatHistory(); 
+    muatHistory();  // tampilan
     console.log(data);
 }
-tombol.addEventListener("click", function() { 
+tombol.addEventListener("click", function() {  // klu tombol diklick fungsi di dlmnya lgsng dijalankan
     var input= inputGram.value.trim(); 
-    var emas = parseFloat(input); 
+    var emas = parseFloat(input); // dari string ke kedalam decimal 
     var nisab= 85; 
     console.log(emas); 
     
-    if( input === "" || isNaN(emas)){ 
-        alert("KOSONG!"); 
-        hasil.textContent = "coba donkkkk"; 
+    if( input === "" || isNaN(emas)){  //  isnan cek apakah input bukan angka
+        alert("KOSONG!"); // maka ada pop up
+        hasil.textContent = "coba donkkkk"; // jika belum zakat maka muncul tampilan 
         return;
     };
     
@@ -55,8 +56,8 @@ tombol.addEventListener("click", function() {
         let zakat = emas*0.025 ;
         let rupiah = zakat*hargaEmasRupiah ; 
         hasil.textContent = `Wajib Zakat Emas ${zakat}gram atau Rp.${rupiah}` 
-        simpanHistory(`${emas}gram maka wajib zakat sebesar Rp.${rupiah.toLocaleString()}`) 
-    }
+        simpanHistory(`${emas}gram maka wajib zakat sebesar Rp.${rupiah.toLocaleString()}`) // menampilkan hasil formt 
+    }                                                           // buat koma biar lbh rapi
     console.log(hasil)
     
     inputGram.value = ""
